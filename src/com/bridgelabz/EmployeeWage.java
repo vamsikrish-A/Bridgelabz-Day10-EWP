7de8bb2727ba46abdfd0d0adf24bb1936e0d68f8
vamsikrish-A <krishnavamsi165@gmail.com>
2021-08-31 00:27:10 +0530

    switch case

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,29 +3,27 @@ package com.bridgelabz;
 public class EmployeeWage {
 
     public static final int WAGE_PER_HOUR = 20;
-    public static final int FULL_DAY_HOUR = 8;
-    public static final int PART_TIME_HOURS = 4;
+    public static final int FULL_DAYS_HOUR = 2;
+    public static final int PART_DAYS_HOURS = 1;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wages. ");
 
-        int partTime = 1;
-        int fullTIme = 2;
+
         int empHours = 0;
 
-        double Checkout = Math.floor(Math.random() * 10) % 3;
-        if (Checkout == fullTIme) {
-            System.out.println("Employee Present.");
-            empHours = FULL_DAY_HOUR;
+        int checkOut = (int)Math.floor(Math.random() * 10) % 3;
+        switch (checkOut) {
+            case FULL_DAYS_HOUR:
+                empHours = 8;
+                break;
+            case PART_DAYS_HOURS:
+                empHours = 4;
+                break;
+            default:
+                empHours = 0;
+        }
 
-        }
-        else if (Checkout == partTime) {
-            System.out.println("part time");
-            empHours = PART_TIME_HOURS;
-        }
-        else {
-            System.out.println("Employee Absent.");
-        }
 
         int dailyWage = empHours * WAGE_PER_HOUR;
         System.out.println("Daily employee Wage::" +dailyWage);
