3d25686c8a15560317b3aed3c98e1892e241187c
vamsikrish-A <krishnavamsi165@gmail.com>
2021-08-30 23:53:21 +0530

    full & part time

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,17 +4,31 @@ public class EmployeeWage {
 
     public static final int WAGE_PER_HOUR = 20;
     public static final int FULL_DAY_HOUR = 8;
+    public static final int PART_TIME_HOURS = 4;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wages. ");
 
-        double Checkout = Math.floor(Math.random() * 10) % 2;
-        if (Checkout == 1) {
+        int partTime = 1;
+        int fullTIme = 2;
+        int empHours = 0;
+
+        double Checkout = Math.floor(Math.random() * 10) % 3;
+        if (Checkout == fullTIme) {
             System.out.println("Employee Present.");
-            int dailyWage = FULL_DAY_HOUR * WAGE_PER_HOUR;
-            System.out.println("Daily employee Wage::" +dailyWage);
+            empHours = FULL_DAY_HOUR;
+
         }
-        else
-            System.out.println("Employee absent.");
+        else if (Checkout == partTime) {
+            System.out.println("part time");
+            empHours = PART_TIME_HOURS;
+        }
+        else {
+            System.out.println("Employee Absent.");
+        }
+
+        int dailyWage = empHours * WAGE_PER_HOUR;
+        System.out.println("Daily employee Wage::" +dailyWage);
     }
+
 }
