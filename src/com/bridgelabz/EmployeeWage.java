c273c05029ed677ba2f4fa4f71a7bded5a030d9d
vamsikrish-A <krishnavamsi165@gmail.com>
2021-08-31 00:51:45 +0530

    multiple compy with different wages

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -6,11 +6,22 @@ public class EmployeeWage {
     public static final int FULL_DAYS_HOUR = 2;
     public static final int PART_DAYS_HOURS = 1;
 
+    private final String company;
+    private final int wagePerHour;
+    private final int numOfWorkingDays;
+    private final int maxHoursPerMonth;
+    private int totalEmpWage;
 
+    public EmployeeWage (String company, int wagePerHour, int numOfWorkingDays, int maxHrsPerMonth) {
+        this.company = company;
+        this.maxHoursPerMonth = maxHrsPerMonth;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.wagePerHour = wagePerHour;
+    }
 
-    public static  int empWageSystem(String company, int wagePerHour, int numOfWorkingDays, int maxHrsPerMonth) {
-        int empHours = 0,  empWagePerMonth = 0, totalHrs = 0, totalWorkingDays = 0;
-        while (totalHrs <= maxHrsPerMonth && totalWorkingDays < numOfWorkingDays) {
+    public void empWageSystem() {
+        int empHours = 0,  totalHrs = 0, totalWorkingDays = 0;
+        while (totalHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
             totalWorkingDays++;
             int checkOut = (int) Math.floor(Math.random() * 10) % 3;
             switch (checkOut) {
@@ -27,21 +38,24 @@ public class EmployeeWage {
             System.out.println("Day#:"+totalWorkingDays + " Emp hr:"+empHours);
         }
 
-        System.out.println("Employee total Hours ::"+totalHrs);
-        int totalEmpWage = totalHrs * wagePerHour;
-        System.out.println("Employee per month::"+totalEmpWage);
-        return totalEmpWage;
+        totalEmpWage = totalHrs * wagePerHour;
 
     }
+    @Override
+    public String toString() {
+        return "Total Emp Wage for Company: " +company+ " is: " +totalEmpWage;
+    }
+
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wages. ");
-        System.out.println("Welcome to Employee Wage system for BIG BAZAR.");
-        empWageSystem("Big Bazar",20,20,100);
-        System.out.println("Welcomr to Employee Wage system for SAMSUNG.");
-        empWageSystem("SAMSUNG",30,25,80);
-        System.out.println("Welcomr to Employee Wage System for NOKIA.");
-        empWageSystem("Nokia", 28,23,105);
+        EmployeeWage samSung = new EmployeeWage("SAMSUNG",20, 25, 90);
+        EmployeeWage noKia = new EmployeeWage("NOKIA", 25, 25, 80);
+
+        samSung.empWageSystem();
+        System.out.println(samSung);
+
+        noKia.empWageSystem();
+        System.out.println(noKia);
 
 
     }
