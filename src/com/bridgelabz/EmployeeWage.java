bc37f8ef856a9cee007efc8f47b892c0bd111c68
vamsikrish-A <krishnavamsi165@gmail.com>
2021-08-30 23:44:03 +0530

    emp present

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,5 +3,11 @@ package com.bridgelabz;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wages. ");
+
+        double Checkout = Math.floor(Math.random() * 10) % 2;
+        if (Checkout == 1)
+            System.out.println("Employee Present.");
+        else
+            System.out.println("Employee absent.");
     }
 }
